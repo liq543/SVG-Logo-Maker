0c1f0ceee26bed18316dddf216327d56c337b33f
Griffin <liq543@gmail.com>
2023-07-25 10:23:35 -0500

    added shape.js

---
+++ shape.js
@@ -0,0 +1,17 @@
+class Shape {
+    constructor() {
+        if (this.constructor === Shape) {
+            throw new Error("Abstract class Shape cannot be instantiated directly");
+        }
+    }
+
+    setColor(color) {
+        this.color = color;
+    }
+
+    render() {
+        throw new Error("Method 'render()' must be implemented");
+    }
+}
+
+module.exports = Shape;
