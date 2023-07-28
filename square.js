91b61f24134705c4ce974959ca5c49a85c89550a
Griffin <liq543@gmail.com>
2023-07-28 10:14:45 -0500

    revert again

--- square.js
+++ square.js
@@ -1,9 +1,9 @@
-const Shape = require('./shape');
+import Shape from './shape.js';
 
 class Square extends Shape {
     render() {
-        return `<rect x="50" y="50" width="200" height="100" fill="${this.color}" />`;
+        return `<rect x="100" y="50" width="100" height="100" fill="${this.color}" />`;
     }
 }
 
-module.exports = Square;
+export default Square;
